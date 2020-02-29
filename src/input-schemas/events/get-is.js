a44c7b764f9348926a8b87b11bc4e39b6615e1af
Bailey Tincher <btincher99@gmail.com>
2020-02-29 14:02:23 -0600

    Add events/get route

--- src/input-schemas/events/get-is.js
+++ src/input-schemas/events/get-is.js
@@ -1,1 +1,21 @@
-export default {};
+export default {
+  type: 'object',
+  required: ['headers', 'pathParameters'],
+  properties: {
+    queryStringParameters: {
+      type: 'object',
+      required: ['event'],
+      additionalProperties: false,
+      properties: {
+        whereEvent: {
+          type: object // TODO[Bailey]: restrict where clause params
+        },
+        limit: {
+          type: 'number',
+          default: 50,
+          maximum: 50
+        }
+      }
+    }
+  }
+};
